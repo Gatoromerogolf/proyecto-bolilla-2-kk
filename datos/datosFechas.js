09e699c12c995c1bd9a56fe79488e17fa7d24c4a
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-03-23 20:19:51 -0300

    sabado 23

--- datos/datosFechas.js
+++ datos/datosFechas.js
@@ -6,6 +6,7 @@ const fechas = [
     {fec:5, dia:'Sábado', diaFecha:24, mesFecha:2, textoFecha:"Sábado 24 de Febrero"},
     {fec:6, dia:'Sábado', diaFecha:2, mesFecha:3, textoFecha:"Sábado 2 de Marzo"},
     {fec:7, dia:'Sábado', diaFecha:9, mesFecha:3, textoFecha:"Sáb 09 Marzo - Cardales"},
+    {fec:8, dia:'Sábado', diaFecha:23, mesFecha:3, textoFecha:"Sábado 23 de Marzo"},
     {fec:90, dia:"Gira", diaFecha:16, mesFecha:3,
     textoFecha:"Sierras 2024"},
     {fec:91, dia:"Gira", diaFecha:16, mesFecha:3,
