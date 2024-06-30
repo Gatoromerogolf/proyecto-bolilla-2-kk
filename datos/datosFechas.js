cfb831985a740cb4f282d246ac493c68eca54992
Gatoromerogolf <ruben.e.garcia@gmail.com>
2024-06-30 11:35:39 -0300

    Merge branch 'main' of https://github.com/Gatoromerogolf/proyecto-bolilla-2-kk

--- datos/datosFechas.js
+++ datos/datosFechas.js
@@ -19,6 +19,7 @@ const fechas = [
     {fec:18, dia:'Sábado', diaFecha:8, mesFecha:6, textoFecha:"Sábado 8 de junio"},
     {fec:19, dia:'Sábado', diaFecha:15, mesFecha:6, textoFecha:"Sábado 15 de junio"},
     {fec:20, dia:'Sábado', diaFecha:22, mesFecha:6, textoFecha:"Sábado 22 de junio"},
+    {fec:21, dia:'Sábado', diaFecha:29, mesFecha:6, textoFecha:"Sábado 29 de junio"},
     {fec:90, dia:"Gira", diaFecha:16, mesFecha:3,
     textoFecha:"Sierras 2024"},
     {fec:91, dia:"Gira", diaFecha:16, mesFecha:3,
